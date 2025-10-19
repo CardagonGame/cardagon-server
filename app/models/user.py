aaede593c01b6fefd6a2156d2a7ef9693a0b1c7a
moisout <maurice@oeger.li>
2025-10-19 20:08:41 +0200

    update settings

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,6 @@ from sqlmodel import Field, SQLModel
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, index=True, max_length=255)
     username: str
-    email: str | None = None
     password_hash: str
